851b9d956129a60a81e63a9c57c2fd5b2f1eb257
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-19 16:55:47 +0200

    alert passoword

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 
+$user_length = $_GET['length'];
+
+function getRandomString($n)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_!?^|@#%$£=';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
+}
 
 
+
+$random_password = getRandomString($user_length);
 
 
 ?>
@@ -13,10 +29,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Password Generetor</title>
 </head>
 
 <body>
+
+    <div class="container">
+        <header>
+            <?php if ($user_length) : ?>
+
+                <div class="alert alert-success mt-3">
+                    La password generata è "<strong> <?= $random_password ?></strong>"
+                </div>
+            <?php endif ?>
+        </header>
+
+        <h1 class="text-center mb-5">STRONG PASSWORD GENERETOR</h1>
+        <form action="#">
+
+            <div class="mb-3 w-25">
+                <label for="number" class="form-label">Inserisci la lunghezza della password:</label>
+                <input type="number" class="form-control" id="number" name="length" min="1" max="100">
+
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+
+        </form>
+
+
+    </div>
+
 
 </body>
 
